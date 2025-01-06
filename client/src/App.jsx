9fd7181fccb79b5c0e467c47c931246a18420ef9
AshishT558 <ashi1j6thomas@gmail.com>
2025-01-06 14:21:45 -0500

    vercel analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
   return (
     <div className="scroll-smooth">
+      <Analytics />
       <Navbar />
       <Outlet />
       <Footer></Footer>
